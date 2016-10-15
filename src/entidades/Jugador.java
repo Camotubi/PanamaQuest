afead6fddcacc905b9bf161ef3ddaf34f530d2d3
-v <camotubi@gmail.com>
2016-10-15 16:22:46 -0500

    Agregada generacion random de preguntas para el preguntador y agregada funcion para resolver preguntas para el jugador

--- src/entidades/Jugador.java
+++ src/entidades/Jugador.java
@@ -6,6 +6,7 @@ public class Jugador {
 	
 	private double dinero;
 	private String nombre;
+	private Pregunta preguntaRecibida;
 	
 
 	public double getDinero() {
@@ -20,6 +21,26 @@ public class Jugador {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
+	public Pregunta getPreguntaRecibida() {
+		return preguntaRecibida;
+	}
+	public void setPreguntaRecibida(Pregunta preguntaRecibida) {
+		this.preguntaRecibida = preguntaRecibida;
+	}
+	
+	public boolean responderPregunta(int respuesta)
+	{
+		if(respuesta == preguntaRecibida.getRespuesta())
+		{
+			dinero = dinero +100;
+			return true;
+		}
+		else
+		{
+			dinero = dinero -100;
+			return false;
+		}
+		
+	}
 
 }
--- src/entidades/Preguntador.java
+++ src/entidades/Preguntador.java
@@ -3,10 +3,12 @@ package entidades;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 
 public class Preguntador {
 	private ArrayList<Pregunta> preguntas = new ArrayList<Pregunta>();
 	private ArrayList<Pregunta> preguntadeCategoriaActual = new ArrayList<Pregunta>();
+	private ArrayList<Integer> preguntasHechas = new ArrayList<Integer>();
 	private void agregarPregunta(String pregunta, String opciones[],int respuesta,int categoria )
 	{
 		preguntas.add(new Pregunta(pregunta,new ArrayList<String>(Arrays.asList(opciones)),respuesta,categoria));
@@ -15,6 +17,7 @@ public class Preguntador {
 	private void cambiarCategoria(int categoria)//extrae todas las preguntas
 	{
 		preguntadeCategoriaActual.clear();
+		preguntasHechas.clear();
 		for(int i = 0; i<preguntas.size();i++)
 		{
 			if(preguntas.get(i).getCategoria() == categoria)
@@ -22,5 +25,26 @@ public class Preguntador {
 				preguntadeCategoriaActual.add(preguntas.get(i));
 			}
 		}
+	}
+	
+	private Pregunta preguntar()
+	{
+		Random generator = new Random();
+		int numeroRand ;
+		boolean pregRepetida = false;
+		do
+		{
+			numeroRand = generator.nextInt(preguntadeCategoriaActual.size());
+			for(int i=0; i<preguntadeCategoriaActual.size() && !pregRepetida;i++)
+			{
+				
+				if(numeroRand == preguntasHechas.get(i))
+				{
+					pregRepetida = true;
+				}
+			}
+		}while(pregRepetida);
+		
+		return preguntadeCategoriaActual.get(numeroRand);
 	}
 }
