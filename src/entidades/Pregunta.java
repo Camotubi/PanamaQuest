2188b388447de072cd38de773b4ca7fdea057746
Carlos Tuñón <camotubi@gmail.com>
2016-10-15 12:50:53 -0500

    Agregadas entidades jugador, pregunta y preguntador con sus resèctivos atributos

---
+++ src/entidades/Pregunta.java
@@ -0,0 +1,11 @@
+package entidades;
+
+import java.util.ArrayList;
+
+public class Pregunta {
+	private String pregunta;
+	private ArrayList<String> opciones = new ArrayList<String>();
+	private int respuesta;
+	private int categoria;
+
+}
