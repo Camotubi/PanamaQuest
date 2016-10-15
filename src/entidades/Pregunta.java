21f9fc04a68e54615602048e561de94f021c32e4
-v <camotubi@gmail.com>
2016-10-15 14:46:38 -0500

    agregadas funciones para agregar preguntas

--- src/entidades/Pregunta.java
+++ src/entidades/Pregunta.java
@@ -3,13 +3,17 @@ package entidades;
 import java.util.ArrayList;
 
 public class Pregunta {
+	public Pregunta(String pregunta, ArrayList<String> opciones, int respuesta, int categoria) {
+
+		this.pregunta = pregunta;
+		this.opciones = opciones;
+		this.respuesta = respuesta;
+		this.categoria = categoria;
+	}
 	private String pregunta;
 	private ArrayList<String> opciones = new ArrayList<String>();
 	private int respuesta;
 	private int categoria;
-	public Pregunta(String pregunta, String opciones[],int respuesta,int categoria )
-	{
-		this
-	}
+
 
 }
--- src/entidades/Preguntador.java
+++ src/entidades/Preguntador.java
@@ -1,5 +1,16 @@
 package entidades;
 
-public class Preguntador {
 
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Preguntador {
+	private ArrayList<Pregunta> preguntas = new ArrayList<Pregunta>();
+	private ArrayList<Pregunta> preguntadeCategoriaActual = new ArrayList<Pregunta>();
+	private void agregarPregunta(String pregunta, String opciones[],int respuesta,int categoria )
+	{
+		
+		preguntas.add(new Pregunta(pregunta,new ArrayList<String>(Arrays.asList(opciones)),respuesta,categoria));
+	}
 }
