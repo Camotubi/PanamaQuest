f9f6672b1c713dc12d5e0010da34e497c005cd56
Roy Henriquez <roy.henriquez@gmail.com>
2016-10-15 14:18:44 -0500

    Agregue los getters y setters en Jugador y Pregunta

--- src/entidades/Pregunta.java
+++ src/entidades/Pregunta.java
@@ -7,9 +7,34 @@ public class Pregunta {
 	private ArrayList<String> opciones = new ArrayList<String>();
 	private int respuesta;
 	private int categoria;
+	
 	public Pregunta(String pregunta, String opciones[],int respuesta,int categoria )
 	{
 		this
+	}
+	public String getPregunta() {
+		return pregunta;
+	}
+	public void setPregunta(String pregunta) {
+		this.pregunta = pregunta;
+	}
+	public ArrayList<String> getOpciones() {
+		return opciones;
+	}
+	public void setOpciones(ArrayList<String> opciones) {
+		this.opciones = opciones;
+	}
+	public int getRespuesta() {
+		return respuesta;
+	}
+	public void setRespuesta(int respuesta) {
+		this.respuesta = respuesta;
+	}
+	public int getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(int categoria) {
+		this.categoria = categoria;
 	}
 
 }
