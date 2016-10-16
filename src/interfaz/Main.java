04e217aabcdbc451e9bd4fe9c14896a201eeec9f
-v <camotubi@gmail.com>
2016-10-15 19:49:00 -0500

    Cambiado if de si la respuesta esta correcta o no en el main por un switch por claridad del codigo y eficiencia

--- src/interfaz/Main.java
+++ src/interfaz/Main.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
 import entidades.Jugador;
-
+import entidades.Preguntador;
 import entidades.Jugador;
 
 public class Main {
@@ -12,6 +12,7 @@ public class Main {
 		
 		ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
 		Jugador jugador = new Jugador();
+		Preguntador preg = new Preguntador();
 		
 		int numJugadores;
 		int controlRespuesta =-1;
@@ -37,19 +38,18 @@ public class Main {
 		do {
 			// que debo poner en respuestas ._.
 			int i =0; 
+			
 			turno = (i+1)%numJugadores;
 			int resp = Integer.parseInt(JOptionPane.showInputDialog(null,"Turno del jugador "+turno+"\nLa pregunta es :"+jugador.getPreguntaRecibida()+"\n Respuesta:\n1- \n2- \n3- \n4- \n0-Retirarse"));
 			contadorPreguntas++;
 			controlRespuesta = jugador.responderPregunta(resp);
-			if(controlRespuesta == 1)
-					JOptionPane.showMessageDialog(null, "Felicidades jugador "+turno+"la respuesta ha sido correcta");			
-			else {
-				if(controlRespuesta == 2)
-					JOptionPane.showMessageDialog(null, "jugador "+turno+"su respuesta fue incorrecta");
-				else {
-					Jugador temph = jugadores.get(turno);
-					JOptionPane.showMessageDialog(null, "El jugador "+temph.getNombre()+" se retiro \n Pregunta en la que se retiro:"+contadorPreguntas+"\n dinero acumulado fue :"+temph.getDinero());
-				}
+			switch(controlRespuesta)
+			{
+			case 1: JOptionPane.showMessageDialog(null, "Felicidades jugador "+turno+"la respuesta ha sido correcta");	 break;
+			case 2: JOptionPane.showMessageDialog(null, "Felicidades jugador "+turno+"la respuesta ha sido correcta");	 break;
+			case 0: Jugador temph = jugadores.get(turno);
+			JOptionPane.showMessageDialog(null, "El jugador "+temph.getNombre()+" se retiro \n Pregunta en la que se retiro:"+contadorPreguntas+"\n dinero acumulado fue :"+temph.getDinero());
+			break;
 			}
 		}while(contadorPreguntas <10 && controlRespuesta !=0 );
 		
