8b697ef2cd8c8b9b3300fc10229e2df337c53214
moises chin <moises.chin1@gmail.com>
2016-10-17 21:08:23 -0500

    Funcion para que al usar un comodin solo salgan 2 preguntas,status : done

--- src/interfaz/Main.java
+++ src/interfaz/Main.java
@@ -11,6 +11,13 @@ import entidades.Preguntador;
 import entidades.Jugador;
 
 public class Main {
+	
+	/*Cosas por hacer
+	 * Cuando los jugadores se retiran, no se retiran en verdad siguen saliendo en el juego ** WIP
+	 * Hacer que aparescan 2 preguntas cuando se usa el comodin  ** done by a good boi
+	 * 
+	 * */
+	
 
 	public static void main(String[] args) {
 		
@@ -23,6 +30,7 @@ public class Main {
 		int turno=0;
 		int resp = 0;
 		int categoriaAct = 1;
+		boolean UsoComodin; //control sobre los comodines para mostrar opciones
 		JOptionPane.showMessageDialog(null, "PanamaQuest 1.0");
 		cantJugador = Integer.parseInt(JOptionPane.showInputDialog(null,"Cuantos jugadores jugaran esta vez?"));
 		
@@ -33,6 +41,7 @@ public class Main {
 		}
 		preg.cambiarCategoria(1);
 		do {
+			UsoComodin =false;  // inicializacion de los usos de comodin
 			if(contadorPreguntas%10==0)
 			{
 				preg.cambiarCategoria(++categoriaAct);
@@ -45,14 +54,23 @@ public class Main {
 				jugadorAct = jugadores.get(turno);
 			}while(jugadorAct.isRetirado());
 			jugadorAct.setPreguntaRecibida(preg.preguntar());
-			resp = Integer.parseInt(JOptionPane.showInputDialog(null,stringPregunta(jugadorAct,contadorPreguntas)));
+			resp = Integer.parseInt(JOptionPane.showInputDialog(null,stringPregunta(jugadorAct,contadorPreguntas,UsoComodin)));
 			contadorPreguntas++;
+			controlRespuesta = jugadorAct.responderPregunta(resp);
+			if(resp == 10) // alternativa si decide usar el   comodin 
+			{
+				JOptionPane.showMessageDialog(null, jugadorAct.getNombre()+ " usó un comodin, le quedan " + jugadorAct.getComodin());
+				UsoComodin = true;
+				resp = Integer.parseInt(JOptionPane.showInputDialog(null,stringPregunta(jugadorAct,contadorPreguntas,UsoComodin)));
+			}
 			controlRespuesta = jugadorAct.responderPregunta(resp);
 			switch(controlRespuesta)
 			{
 			case 1: JOptionPane.showMessageDialog(null, "Felicidades " + jugadorAct.getNombre()+"la respuesta ha sido correcta");break;
 			case 2: JOptionPane.showMessageDialog(null, jugadorAct.getNombre()+"su respuesta ha sido incorrecta");break;
-			case 3: JOptionPane.showMessageDialog(null, jugadorAct.getNombre()+ " usó un comodin, le quedan " + jugadorAct.getComodin());break;
+			case 3: //JOptionPane.showMessageDialog(null, jugadorAct.getNombre()+ " usó un comodin, le quedan " + jugadorAct.getComodin());
+		
+			break;
 			case 0: Jugador temph = jugadores.get(turno);
 			JOptionPane.showMessageDialog(null, "El jugador "+temph.getNombre()+" se retiro \n Pregunta en la que se retiro:"+contadorPreguntas+"\n dinero acumulado fue :"+temph.getDinero());
 			break;
@@ -67,6 +85,8 @@ public class Main {
 	{	
 		BufferedReader br = null;
 		try {
+			int conttemp = 0; // pruebas para reducir opciones
+			
 			String sCurrentLine;
 			String tempPreg = null;
 			int tempRespIndex = 0;
@@ -113,20 +133,52 @@ public class Main {
 		}
 		return preguntador;
 	}
-	public static String stringPregunta(Jugador jugador,int contador)
+	public static String stringPregunta(Jugador jugador,int contador,boolean usoComo)
 	{
-		StringBuilder strBuild = new StringBuilder();
-		strBuild.append("Turno de "+jugador.getNombre()+"         Dinero:" + jugador.getDinero()+"\nPregunta #"+contador+":" +jugador.getPreguntaRecibida().getPregunta()+"\n Opciones:");
+		int contadordepreguntas=0;
+		boolean respencontrada = false;
+		boolean pregbasura = false;
 		
-		for(int i=0;i<jugador.getPreguntaRecibida().getOpciones().size();i++)
-		{
-			strBuild.append("\n"+(i+1)+"-"+jugador.getPreguntaRecibida().getOpciones().get(i));
+		int resp=jugador.getPreguntaRecibida().getRespuesta();
+		StringBuilder strBuild = new StringBuilder(); // objetos de tipo string que hacen mas facil concatenar  varios strings, aka lo que se hace aqui abajo
+		strBuild.append("Turno de "+jugador.getNombre()+"         Dinero:" + jugador.getDinero()+"\nPregunta #"+contador+":" +jugador.getPreguntaRecibida().getPregunta()+"\n Opciones:");
+		if(usoComo == true ) {
+			
+			
+			for(int i=0;i<jugador.getPreguntaRecibida().getOpciones().size() ||  contadordepreguntas < 2 && respencontrada == true;i++)
+			{
+				
+				if(jugador.getPreguntaRecibida().getOpciones().get(i) == jugador.getPreguntaRecibida().getOpciones().get(resp))
+				{
+					contadordepreguntas++;
+					respencontrada = true;
+					strBuild.append("\n"+(i+1)+"-"+jugador.getPreguntaRecibida().getOpciones().get(i));
+				}
+				if(jugador.getPreguntaRecibida().getOpciones().get(i) != jugador.getPreguntaRecibida().getOpciones().get(resp) && pregbasura != true)
+				{
+					contadordepreguntas++;
+					pregbasura = true;
+					strBuild.append("\n"+(i+1)+"-"+jugador.getPreguntaRecibida().getOpciones().get(i));
+				}
+					
+				
+			}
+			
+		
+			strBuild.append("\n0-Retirarse");
 		}
-		if(jugador.getComodin()>0)
-		{
-			strBuild.append("\n10-Usar comodin");
+		else {
+			for(int i=0;i<jugador.getPreguntaRecibida().getOpciones().size();i++)
+			{
+				strBuild.append("\n"+(i+1)+"-"+jugador.getPreguntaRecibida().getOpciones().get(i));
+			}
+			if(jugador.getComodin()>0)
+			{
+				strBuild.append("\n10-Usar comodin");
+				
+			}
+			strBuild.append("\n0-Retirarse");
 		}
-		strBuild.append("\n0-Retirarse");
-		return strBuild.toString();
+			return strBuild.toString();
 	}
 }
